7e0cba8b838e5d82c4df5044d46e6ba55aa744a0
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 11:29:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
+const Discord = require("discord.js");
+const client = new Discord.Client()
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if(message.content == '$mb') {
+    const embed = new Discord.RichEmbed()
+    .addField(`حالة الأعضاء 🔋`,'-',   true)
+.addField(`💚 اونلاين :   ${message.guild.members.filter(m=>m.presence.status == 'online').size}`,'-',   true)
+.addField(`❤ مشغول :     ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`,'-',   true)
+.addField(`💛 خامل :      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}`,'-',   true)
+.addField(`🖤 اوفلاين :   ${message.guild.members.filter(m=>m.presence.status == 'offline').size}`,'-',  true)
+.addField(`💙   الكل :  ${message.guild.memberCount}`,'-',   true)
+         message.channel.send({embed})
+
+    }
+  })
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
-setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
-}, 30)
-})
 
-client.login(process.env.BOT_TOKEN);
+  
+client.login('NNTI2OTcyNzA3NDAxNzYwNzc4.Dwi_9w.B45OIoNbOxFbnEyGCPB2lYYNlMET')
